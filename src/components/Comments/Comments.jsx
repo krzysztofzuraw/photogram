15fc143f4ac7bc880e6f30804dcf32369ae7b1b8
Krzysztof Żuraw <krzysztof.zuraw@gmail.com>
2017-11-01 17:00:47 +0100

    Allow adding & seeing Comments

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -4,47 +4,46 @@ import { connect } from "react-redux";
 import { Comment, Header, Form, Button } from "semantic-ui-react";
 import { addComment } from "../../actions/index.js";
 
-// function Comments() {
-//   return (
-//     <Comment.Group>
-//       <Header as="h3" dividing>
-//         Comments
-//       </Header>
-
-//       <Comment>
-//         <Comment.Content>
-//           <Comment.Author as="a">Matt</Comment.Author>
-//           <Comment.Metadata>
-//             <div>Today at 5:42PM</div>
-//           </Comment.Metadata>
-//           <Comment.Text>How artistic!</Comment.Text>
-//           <Comment.Actions>
-//             <Comment.Action>Reply</Comment.Action>
-//           </Comment.Actions>
-//         </Comment.Content>
-//       </Comment>
-//     </Comment.Group>
-//   );
-// }
-
 class Comments extends React.Component {
   constructor(props) {
     super(props);
-
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    this.props.addComment("anon", "You are cool!");
+    this.props.addComment(this.refs.author.value, this.refs.comment.value);
+    this.refs.author.value = "";
+    this.refs.comment.value = "";
   }
 
   render() {
     return (
-      <Form reply onSubmit={this.handleSubmit}>
-        <Form.TextArea />
-        <Button content="Add Reply" labelPosition="left" icon="edit" primary />
-      </Form>
+      <Comment.Group>
+        {this.props.comments.map((comment, key) => (
+          <Comment key={key}>
+            <Comment.Content>
+              <Comment.Author>{comment.author}</Comment.Author>
+              <Comment.Text>{comment.text}</Comment.Text>
+            </Comment.Content>
+          </Comment>
+        ))}
+
+        <Form onSubmit={this.handleSubmit} reply>
+          <Form.Field>
+            <input placeholder="Name" type="text" required ref="author" />
+          </Form.Field>
+          <Form.Field>
+            <input placeholder="Comment" type="text" ref="comment" />
+          </Form.Field>
+          <Button
+            content="Add Reply"
+            labelPosition="left"
+            icon="edit"
+            primary
+          />
+        </Form>
+      </Comment.Group>
     );
   }
 }
