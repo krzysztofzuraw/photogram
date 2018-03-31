6cfa13f44aa94d7bdd14284cce95cf795939ba0a
Krzysztof Żuraw <krzysztof.zuraw@shipwallet.com>
2018-03-31 18:30:53 +0200

    Add unit tests for Erro & Header

---
+++ src/components/Header/Header.test.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { shallow } from "enzyme";
+
+import Header from "./Header";
+
+describe("Header Component", () => {
+  it("renders correctly", () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
